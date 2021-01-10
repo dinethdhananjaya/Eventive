acb22a0cfadeff021b8a4aec5363118391979c1c
HMDTAbeywardana <69184071+HMDTAbeywardana@users.noreply.github.com>
2021-01-10 23:36:50 +0530

    event_planner_activity_main add

---
+++ app/src/main/java/com/madgroup/evantive/EventPlanner/EventPlannerMainActivity.java
@@ -0,0 +1,16 @@
+package com.madgroup.evantive.EventPlanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.madgroup.evantive.R;
+
+public class EventPlannerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_planner_activity_main);
+    }
+}
